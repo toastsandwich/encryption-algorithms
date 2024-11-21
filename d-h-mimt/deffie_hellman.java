d52c1e9448b68fd45b29307696b90d723d18da8c
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2024-11-21 12:25:23 +0000

    organise

--- d-h-mimt/deffie_hellman.java
+++ d-h-mimt/deffie_hellman.java
@@ -1,3 +1,7 @@
 public class deffie_hellman {
+    // large prime number
+    public int p;
+    public int g;
+
     
 }
---
+++ d-h-mimt/interceptor.java
@@ -0,0 +1,4 @@
+public class interceptor {
+    private int secret;
+    
+}
