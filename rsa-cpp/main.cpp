9e562315c973a48fee91e8b70718b19b24dc51ee
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2024-11-21 18:29:31 +0530

    Update main.cpp

--- rsa-cpp/main.cpp
+++ rsa-cpp/main.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
+#include <vector>
 
 #include "helper.h"
 
-int main() {
-  key::PublicKey pb(7, 11);
+int main(const int argc, const char *argv[]) {
+  key::PublicKey pb(7, 1009);
   auto cipher = pb.Encrypt(9);
   std::cout << "cipher: " << cipher << std::endl;
+
+  if (argc != 2) {
+    std::cerr << "usage: rsa <message>" << std::endl;
+    return 1;
+  }
+
+  const std::string message = argv[1];
+  std::vector<unsigned long long> message_vec;
+
+  for (const auto c: message) {
+    auto m = static_cast<unsigned long long>(c);
+    message_vec.push_back(m);
+  }
+
+  std::vector<unsigned long long> encrypted;
+  for (const auto &m: message_vec) {
+    const auto e = pb.Encrypt(m);
+    encrypted.push_back(e);
+  }
+
+  for(const auto &e: encrypted) {
+    std::cout << (e) << " ";
+  } std::cout << std::endl;
+
   key::PrivateKey pk(&pb);
-  auto message = pk.Decrypt(cipher);
-  std::cout << "message: " << message << std::endl;
+  auto mssg = pk.Decrypt(cipher);
+  std::cout << "message: " << mssg << std::endl;
+  
+  std::vector<unsigned long long> plain;
+  for (const auto &e: encrypted) { 
+    const auto m = pk.Decrypt(e);
+    plain.push_back(m);
+  }
+
+  for(const auto &e: plain) {
+    std::cout << static_cast<char>(e);
+  } std::cout << std::endl;
+
   return 0;
 }
